6ff4c1d8a297a9992037cb004118ad4946aeaf6a
aqil2514 <muhamadaqil383@gmail.com>
2026-04-15 11:41:37 +0700

    Update server

--- server/src/app/activities/services/activities.service.ts
+++ server/src/app/activities/services/activities.service.ts
@@ -1,20 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ActivitiesFetcherHelper } from './helpers/activities-fetcher-helper.service';
-import { ActivityResponse } from 'src/app/image-upload/interfaces/ai-screen-report.interface';
+import { ActivityData } from '../interface/activities_data.interface';
 
 @Injectable()
 export class ActivitiesService {
   constructor(private readonly helper: ActivitiesFetcherHelper) {}
 
-  async getActivityData(
-    userId: string,
-    date: string,
-  ): Promise<ActivityResponse> {
+  async getActivityData(userId: string, date: string): Promise<ActivityData[]> {
     const summariesData = await this.helper.getSessionActivityByUserId(
       userId,
       date,
     );
-    const summaryTime = await this.helper.getSummaryTime(userId, date);
 
     const allRawIds = summariesData.flatMap((data) => data.raw_ids);
 
@@ -22,18 +18,38 @@ export class ActivitiesService {
 
     const data = this.helper.mapToActivityData(allReports, summariesData);
 
-    return {
-      activities: data,
-      dailySummaryTime: summaryTime.dailySummaryTime,
-      weeklySummaryTime: summaryTime.weeklySummaryTime,
-    };
+    return data;
   }
+
+// TODO : Nanti update yang ini
+  //   async getActivityData(
+  //   userId: string,
+  //   date: string,
+  // ): Promise<ActivityResponse> {
+  //   const summariesData = await this.helper.getSessionActivityByUserId(
+  //     userId,
+  //     date,
+  //   );
+  //   const summaryTime = await this.helper.getSummaryTime(userId, date);
+
+  //   const allRawIds = summariesData.flatMap((data) => data.raw_ids);
+
+  //   const allReports = await this.helper.getRawActivityRawIds(allRawIds);
+
+  //   const data = this.helper.mapToActivityData(allReports, summariesData);
+
+  //   return {
+  //     activities: data,
+  //     dailySummaryTime: summaryTime.dailySummaryTime,
+  //     weeklySummaryTime: summaryTime.weeklySummaryTime,
+  //   };
+  // }
 
   async getDailyActivity(userId: string, date: string) {
     return await this.helper.getDailyActivity(userId, date);
   }
-
-  async getDailyActivityPerCategory(userId: string, date: string) {
-    return await this.helper.getDailyActivityPerCategory(userId, date);
+  
+  async getDailyActivityPerCategory(userId: string, date: string){
+    return await this.helper.getDailyActivityPerCategory(userId, date)
   }
 }
